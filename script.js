cee4e452e7ef07b8fdd504e488e90087689d88c2
Manish Kumar Barman <manishkumarbarman111@gmail.com>
2024-11-24 05:07:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
-function myFunction() {
-    var element = document.body;
-    element.classList.toggle("dark-mode");
-}
+document.addEventListener("DOMContentLoaded", function () {
+    const project = document.getElementById("project")
+    const vision = document.getElementById("vision")
+    const about = document.getElementById("about")
+
+    project.addEventListener("click", function () {
+    });
+    vision.addEventListener("click", function () {
+       
+    });
+    about.addEventListener("click", function () {
+    });
+});
