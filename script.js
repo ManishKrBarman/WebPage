bc6514d4278ca7370560fc1f5d5e906d504647db
Manish Kumar Barman <manishkumarbarman111@gmail.com>
2024-11-24 22:19:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,1 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const project = document.getElementById("project")
-    const vision = document.getElementById("vision")
-    const about = document.getElementById("about")
-
-    project.addEventListener("click", function () {
-    });
-    vision.addEventListener("click", function () {
-       
-    });
-    about.addEventListener("click", function () {
-    });
-});
+document.addEventListener("DOMContentLoaded", function () {});
